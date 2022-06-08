f5f698695d6f53f517c10fdeb6d35e9e38a91043
DSlundro <dragan.savic95@outlook.it>
2022-06-08 15:36:14 +0200

    add bonus

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $msg = 'Hello World';
     return view('home', compact('msg'));
-});
+})->name('home');
+Route::get('/about-me', function () {
+    $msg = 'About Me';
+    return view('about-me', compact('msg'));
+})->name('about-me');
+Route::get('/library', function () {
+    $msg = 'Library';
+    return view('library', compact('msg'));
+})->name('library');
+Route::get('/blog', function () {
+    $msg = 'Blog';
+    return view('blog', compact('msg'));
+})->name('blog');
+Route::get('/contact', function () {
+    $msg = 'Contact';
+    return view('contact', compact('msg'));
+})->name('contact');
